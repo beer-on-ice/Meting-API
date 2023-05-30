c28651ce177e36a4b2697e0e26411d409d0634b7
LiJin <jin.l@medicalsystem.cn>
2023-05-30 16:03:23 +0800

    feat: fix

--- src/providers/ytmusic/index.js
+++ src/providers/ytmusic/index.js
@@ -1,11 +1,9 @@
-import config from "../../config.js"
 const support_type = ['song', 'playlist']
-const YT_API = config.YT_API
 const handle = async (type, id, cookie = '') => {
     let result
     const query = `?type=${type}&id=${id}`
     if (support_type.includes(type)) {
-        result = await fetch(YT_API + query)
+        result = await fetch('https://xiaoneng.ml/meting/api' + query)
         result = await result.json()
     } else {
         result = -1
