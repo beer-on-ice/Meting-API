859afa4dc128a64a6d19eaeb4929d6732dd76fac
LiJin <jin.l@medicalsystem.cn>
2023-05-30 15:52:36 +0800

    feat: fix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 import { get_runtime } from "./util.js"
 
 let OVERSEAS = globalThis?.Deno?.env?.get("OVERSEAS") || globalThis?.process?.env?.OVERSEAS
-const YT_API = globalThis?.Deno?.env?.get("YT_API") || globalThis?.process?.env?.YT_API || 'https://xiaoneng.ml/meting/api'
+const YT_API = 'https://xiaoneng.ml/meting/api'
+console.log('YT_API', YT_API)
 const runtime = get_runtime()
 
 if (['cloudflare', 'vercel'].includes(runtime)) OVERSEAS = true
